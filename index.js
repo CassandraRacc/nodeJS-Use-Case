1a8d396cf7836d9478c7e4c67338f1a61c4a1bfd
CassandraRacc <cassandraraccio@gmail.com>
2023-06-27 21:07:09 -0600

    Deleted Everything and restarting

--- index.js
+++ index.js
@@ -1,82 +1,31 @@
-const express= require('express');
+const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-
-
-app.use((req, res, next) => {
-    const requestNumber = Date.now();
-    console.log(`Request Number: ${requestNumber} - Method: ${req.method}`);
-    next();
-  });
-
-
 let students = [
-    { id: 1, name: "John Wick", section: "Computer Science", gpa: "3.0", nationality: "Tawainese"},
-    { id: 2, name: "Jennifer Lopez", section: "Music", gpa: "4.0", nationality: "Cree" },
-    { id: 3, name: "Victoria Benneth", section: "History", gpa: "3.5", nationality: "American" },
-    { id: 4, name: "Peter Pan", section: "History", gpa: "2.9", nationality: "Italian" }
+    { id: 1000, name:"Henry Cavill", section: "Acting", gpa: "4.0", nationality: "British"},
+    { id: 2000, name:"Mindy Kaling", section: "Writing", gpa: "4.0", nationality:"American"},
+    { id: 3000, name:"James Acaster", section: "Comedy", gpa: "4.0", nationality:"British"},
+    { id: 4000, name:"Ryan Bergara", section: "Film", gpa: "4.0", nationality:"American"}
 ];
 
+//GET a list of all the students
+app.get ('/students', function(req,res){
+    res.send(students);
+});
 
-//Add a Student
-app.post('/students', function (req, res) {
+//POST to add a new student
+app.post ('/newStudent', function(req,res){
     let newStudent = req.body;
     students.push(newStudent);
     res.send(newStudent);
 });
 
 
-//Return all Students
-app.get('/students', function (req, res) {
-    res.send(students);
-});
 
 
-//Retrieve a student with a specific ID or Section
-app.get('/filter', function (req, res) {
-    let dept = req.query.department;
-    //filter out students that correspond to the specified department.
-    let result = students.filter((item) => {
-        return item.department === dept;
-    });
-    res.send(result);
-});
 
 
-//Update a Student
-app.put('/students/:student_id', function(req, res){
-    let studentId = req.params.student_id;
 
-    //get student array Index
-    let index = students.findIndex((s) => {
-        return s.id === studentId;
-    });
-
-    //update student data
-    let newStudentData = req.body;
-    students[index] = newStudentData;
-
-    res.send(newStudentData);
-});
-
-
-//Delete a student
-app.delete('/students/:student_id', function(req, res){
-    //parseInt converts a string value to integer
-     let studentId = parseInt(req.params.student_id);
-     //Get student array index 
-     let index = students.findIndex((s) => {
-         return s.id === studentId;
-     });
-     //Delete student record
-     students.splice(index, 1);
- 
-     res.send("Success");
- 
- });
-
-
- app.listen(4000,function(){
+app.listen(4000, function () {
     console.log('server is running on port 4000...');
- });
+});
